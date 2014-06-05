194c5aa0504f7016ee6c016bf04124bfc28b391a
zcy <zcy920225@gmail.com>
2014-06-05 18:29:19 +0800

    (Windows only) Test static linkage

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,42 @@
 #ifdef _MSC_VER
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 #pragma comment(lib, "ws2_32.lib")
-#pragma comment(lib, "v8_base.lib")
-#pragma comment(lib, "v8_snapshot.lib")
-#pragma comment(lib, "v8_nosnapshot.lib")
 #pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "Opengl32.lib")
+#pragma comment(lib, "Imm32.lib")
+#pragma comment(lib, "D3d9.lib")
+#pragma comment(lib, "dxguid.lib")
+#include <d3d9.h>
+#include <QtCore/qplugin.h>
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
+#ifdef _DEBUG
 #pragma comment(lib, "Qt5Cored.lib")
 #pragma comment(lib, "Qt5Guid.lib")
 #pragma comment(lib, "Qt5Widgetsd.lib")
-#endif
+#pragma comment(lib, "v8_based.lib")
+#pragma comment(lib, "v8_snapshotd.lib")
+#pragma comment(lib, "v8_nosnapshotd.lib")
+#pragma comment(lib, "libEGLd.lib")
+#pragma comment(lib, "libGLESv2d.lib")
+#pragma comment(lib, "preprocessord.lib")
+#pragma comment(lib, "translatord.lib")
+#pragma comment(lib, "Qt5PlatformSupportd.lib")
+#pragma comment(lib, "platforms/qwindowsd.lib")
+#else
+#pragma comment(lib, "v8_base.lib")
+#pragma comment(lib, "v8_snapshot.lib")
+#pragma comment(lib, "v8_nosnapshot.lib")
+#pragma comment(lib, "Qt5Core.lib")
+#pragma comment(lib, "Qt5Gui.lib")
+#pragma comment(lib, "Qt5Widgets.lib")
+#pragma comment(lib, "libEGL.lib")
+#pragma comment(lib, "libGLESv2.lib")
+#pragma comment(lib, "preprocessor.lib")
+#pragma comment(lib, "translator.lib")
+#pragma comment(lib, "Qt5PlatformSupport.lib")
+#pragma comment(lib, "platforms/qwindows.lib")
+#endif // DEBUG
+#endif // _MSC_VER
 
 #include "JSE.h"
 #include "JSEApplication.h"
